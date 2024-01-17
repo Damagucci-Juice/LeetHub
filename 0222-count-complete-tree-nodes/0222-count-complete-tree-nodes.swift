0f112ab81049c95a79afe34935baf438881a94d0
Damagucci-Juice <50472122+Damagucci-Juice@users.noreply.github.com>
2024-01-17 10:58:42 +0900

    Time: 75 ms (41.84%), Space: 17.5 MB (51.02%) - LeetHub

---
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.swift
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func countNodes(_ root: TreeNode?) -> Int {
+        var sum = 0
+        func traversal(_ root: TreeNode?) {
+            if let root = root { 
+                sum += 1
+                traversal(root.left)
+                traversal(root.right)
+            }
+        }
+        traversal(root)
+        return sum
+    }
+}
