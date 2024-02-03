f9e08cf7452176a5d2dca62bb2a26a1614ebb8f0
Damagucci-Juice <50472122+Damagucci-Juice@users.noreply.github.com>
2024-02-03 23:59:27 +0900

    Time: 86 ms (5.07%), Space: 16.7 MB (11.73%) - LeetHub

---
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.swift
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func levelOrder(_ root: TreeNode?) -> [[Int]] {
+        guard let root = root else { return [] }
+        let height = height(root)
+        var ans: [[Int]] = Array(repeating: [], count: height)
+
+        func dfs(_ node: TreeNode?, _ cur: Int, _ level: Int) { 
+            guard let node = node else { return }
+
+            if cur == 1 {
+                ans[level-1].append(node.val)
+            } else if cur > 1 {
+                dfs(node.left, cur-1, level)
+                dfs(node.right, cur-1, level)
+            }
+        }
+
+        for i in 1...height {
+            dfs(root, i, i)
+        }
+        return ans
+    }
+
+    func height(_ node: TreeNode?) -> Int {
+        guard let node = node else { return 0 }
+
+        var lH = height(node.left)
+        var rH = height(node.right)
+
+        if lH > rH {
+            lH += 1 
+        } else {
+            rH += 1 
+        }
+
+        return max(lH, rH)
+    }
+}
